7e956b0592ac829e4caedbaeb9a8058b3cab38ab
Pyush yadav <pyushyadav2004@gmail.com>
2025-08-07 00:10:54 +0530

    day 2 vidtube

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectioninstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMongoDB connected! DB HOST: ${connectioninstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
